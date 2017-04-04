0ecddfc158e39884767ac23375f5fa5ffd150ca1
aliciasavelly <alicia.savelly@gmail.com>
2017-04-04 13:16:49 -0700

    add email to identities table and form to page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404190522) do
+ActiveRecord::Schema.define(version: 20170404200602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170404190522) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
   create_table "personalities", force: :cascade do |t|
