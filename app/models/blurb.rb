0380ddab764c908a081e3906844691adde22c6b9
Elan Weinstock <elanweinstock@Elans-MacBook-Pro.local>
2017-04-03 20:36:03 -0700

    add blurb model

--- app/models/blurb.rb
+++ app/models/blurb.rb
@@ -1,2 +1,35 @@
 class Blurb < ApplicationRecord
+  validates :body, :user_id, :analysis,  presence: true
+  validates :body, uniqueness: { scope: :user_id }
+
+  before_validation :ensure_analysis
+
+
+  def new_analysis
+    options = {
+      basic_auth:{
+        :username=>ENV["WATSON_ID"],
+        :password=>ENV["WATSON_PASSWORD"]
+      },
+      headers:{
+        "Content-Type" => "text/plain"
+      },
+      body: {
+        :body => self.body
+      }
+    }
+
+    full_response = HTTParty.post("https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19", options)
+
+    full_response.body # Parsed body
+  end
+
+  private
+  def ensure_analysis
+    self.analysis ||= new_analysis
+  end
+
+
+
+
 end
