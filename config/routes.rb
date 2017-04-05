1651f549edb63adbfe663867f0caaf9ed9c77586
aliciasavelly <alicia.savelly@gmail.com>
2017-04-05 10:01:13 -0700

    shift around routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
 
-  get 'auth/:provider/callback', to: 'sessions#create'
-  post '/auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
-  resources :identities
 
   namespace :api, defaults: {format: :json} do
+    get 'auth/:provider/callback', to: 'sessions#create'
+    post '/auth/:provider/callback', to: 'sessions#create'
+    get 'auth/failure', to: redirect('/')
+    get 'signout', to: 'sessions#destroy', as: 'signout'
+    resources :sessions, only: [:create, :destroy]
+    resources :identities
     resources :blurbs, only: [:create, :show, :index]
     resources :personalities, only: [:show, :update]
   end
