7ce0359d013567fbfd80184c0302fb9eb46c7156
aliciasavelly <alicia.savelly@gmail.com>
2017-04-05 07:40:45 -0700

    edit protect_from_forgery in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-  protect_from_forgery prepend: true
+  protect_from_forgery with: :exception, prepend: true
   helper_method :current_user
 
   def current_user
