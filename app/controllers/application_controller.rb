d355c420fd646b4bf2ef6aec20a0386ae8f65790
Elan Weinstock <elanweinstock@Elans-MacBook-Pro.local>
2017-04-05 17:59:57 -0700

    login demo works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
+    debugger
     # session[:user_id] = nil
     @current_user ||= User.find_by_uid(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
+
     !!current_user
   end
 
