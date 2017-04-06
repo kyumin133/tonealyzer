1128595369942876385d813541bb907b92168ce1
Kyumin Lee <kyumin133@gmail.com>
2017-04-06 13:05:43 -0700

    fix merge conflicts in root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -5,10 +5,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from "./app";
 import Splash from "./splash";
 import DashboardContainer from "./dashboard/dashboard_container";
-<<<<<<< HEAD
 import BlurbInputContainer from "./blurb_input/blurb_input_container";
-=======
->>>>>>> 20c605770ff2c73e4035b242daca6b385a03dc36
 import ResultsContainer from "./results/results_container";
 
 const Root = ({ store }) => {
@@ -17,10 +14,7 @@ const Root = ({ store }) => {
       <Route path="/" component={ App } >
         <IndexRoute component={ Splash } />
         <Route path="/home" component={ DashboardContainer } />
-<<<<<<< HEAD
         <Route path="/newBlurb" component={ BlurbInputContainer } />
-=======
->>>>>>> 20c605770ff2c73e4035b242daca6b385a03dc36
         <Route path="/results/:blurbId" component={ ResultsContainer } />
       </Route>
     </Router>
