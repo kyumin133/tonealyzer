3479c7fe21ee62f4180aa79f5f43ef998cb0548d
Kyumin Lee <kyumin133@gmail.com>
2017-04-06 12:55:42 -0700

    document-level analysis and hover box

--- frontend/components/results/document_results.jsx
+++ frontend/components/results/document_results.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import { BarHorizontalChart } from "react-d3-basic";
+const DESCRIPTIONS = {
+  anger: "Likelihood of being perceived as angry.",
+  disgust: "Likelihood of being perceived as disgusted.",
+  fear: "Likelihood of being perceived as scared.",
+  joy: "Likelihood of being perceived as happy.",
+  sadness: "Likelihood of being perceived as sad.",
+  analytical: "Likelihood of being perceived as intellectual, rational, systematic, emotionless, or impersonal.",
+  confident: "Likelihood of being perceived as assured, collected, hopeful, or egotistical.",
+  tentative: "Likelihood of being perceived as questionable, doubtful, limited, or debatable.",
+  openness_big5: "Likelihood of being perceived as open to experiences for a variety of activities.",
+  conscientiousness_big5: "Likelihood of being perceived as someone who would act in an organized or thoughtful way.",
+  extraversion_big5: "Likelihood of being perceived as someone who would seek stimulation in the company of others.",
+  agreeableness_big5: "Likelihood of being perceived as compassionate and cooperative towards others.",
+  emotional_range_big5: "Likelihood of being perceived as someone sensitive to the environment."
+}
 
 class DocumentResults extends React.Component {
   constructor(props) {
@@ -47,7 +62,7 @@ class DocumentResults extends React.Component {
                 "tone_name": "Analytical"
               },
               {
-                "score": 0,
+                "score": 0.138352,
                 "tone_id": "confident",
                 "tone_name": "Confident"
               },
@@ -107,7 +122,10 @@ class DocumentResults extends React.Component {
     }
 
     this.state = {
-      dataSets
+      dataSets,
+      hoverBox: {
+        class: "hidden"
+      }
     }
 
     this.y = (d) => {
@@ -123,44 +141,141 @@ class DocumentResults extends React.Component {
 
 
   }
+
+  showHover(e, chartIndex) {
+    let index = $(e.currentTarget).index();
+    let offset = $(e.currentTarget).offset();
+    let field = this.state.dataSets[chartIndex][index];
+
+    let value = Math.round(1000 * field.score) / 1000;
+
+    let likelihood = " (unlikely)";
+    let likelihoodClass = "unlikely";
+
+    if (value >= 0.75) {
+      likelihood = " (very likely)";
+      likelihoodClass = "very-likely";
+    } else if (value >= 0.5) {
+      likelihood = " (likely)";
+      likelihoodClass = "likely";
+    }
+
+
+    this.setState({
+      hoverBox: {
+        name: field.tone_name,
+        value: `${value} ${likelihood}`,
+        likelihoodClass,
+        description: DESCRIPTIONS[field.tone_id],
+        class: "hover-box",
+        style: {
+          left: offset.left,
+          top: offset.top + parseInt($(e.currentTarget).attr("height")) + 5
+        }
+      }
+    });
+  }
+
+  componentDidMount() {
+    if (!!this.blurb) {
+      $(".results-emotion .bar").off();
+      $(".results-language .bar").off();
+      $(".results-social .bar").off();
+
+      $(".results-emotion .bar").mouseenter((e) => {
+        this.showHover(e, 0);
+      });
+
+      $(".results-language .bar").mouseenter((e) => {
+        this.showHover(e, 1);
+      });
+
+      $(".results-social .bar").mouseenter((e) => {
+        this.showHover(e, 2);
+      });
+
+      $(".bar").mouseleave((e) => {
+        this.setState({
+          hoverBox: {
+            class: "hidden-hover-box"
+          }
+        })
+      });
+    }
+  }
+
   render() {
+    let hoverBox = this.state.hoverBox;
+
     return <div className="dashboard-page">
-      <BarHorizontalChart
-        margins={{left: 60, right: 60, top: 60, bottom: 60}}
-        data={this.state.dataSets[0]}
-        width={300}
-        height={300}
-        chartSeries={this.chartSeries}
-        y={this.y}
-        yScale = {'ordinal'}
-        showXGrid={false}
-        showYGrid={false}
-        showLegend={false}
-      />
-    <BarHorizontalChart
-        margins={{left: 60, right: 60, top: 60, bottom: 60}}
-        data={this.state.dataSets[1]}
-        width={300}
-        height={300}
-        chartSeries={this.chartSeries}
-        y={this.y}
-        yScale = {'ordinal'}
-        showXGrid={false}
-        showYGrid={false}
-        showLegend={false}
-      />
-    <BarHorizontalChart
-        margins={{left: 60, right: 60, top: 60, bottom: 60}}
-        data={this.state.dataSets[2]}
-        width={300}
-        height={300}
-        chartSeries={this.chartSeries}
-        y={this.y}
-        yScale = {'ordinal'}
-        showXGrid={false}
-        showYGrid={false}
-        showLegend={false}
-      />
+        <div className={hoverBox.class} style={hoverBox.style}>
+          <div className="hover-header">
+            <span className="hover-name">{hoverBox.name}</span>
+            <span className={hoverBox.likelihoodClass}>{hoverBox.value}</span>
+          </div>
+          <div className="hover-description">{hoverBox.description}</div>
+        </div>
+        <div className="chart-wrapper">
+          <div className="results-title-wrapper">
+            <span className="results-title">Emotion</span>
+          </div>
+          <BarHorizontalChart
+            margins={{left: 60, right: 0, top: 0, bottom: 60}}
+            data={this.state.dataSets[0]}
+            width={270}
+            height={300}
+            chartSeries={this.chartSeries}
+            xTicks={[6]}
+            xDomain={[0, 1]}
+            y={this.y}
+            yScale = {'ordinal'}
+            showXGrid={false}
+            showYGrid={false}
+            showLegend={false}
+            svgClassName={"results-emotion"}
+          />
+      </div>
+      <div className="chart-wrapper">
+        <div className="results-title-wrapper">
+          <span className="results-title">Language Style</span>
+        </div>
+        <BarHorizontalChart
+            margins={{left: 80, right: 0, top: 0, bottom: 60}}
+            data={this.state.dataSets[1]}
+            width={290}
+            height={300}
+            chartSeries={this.chartSeries}
+            xTicks={[6]}
+            xDomain={[0, 1]}
+            yTickSize={[100]}
+            y={this.y}
+            yScale = {'ordinal'}
+            showXGrid={false}
+            showYGrid={false}
+            showLegend={false}
+            svgClassName={"results-language"}
+          />
+      </div>
+      <div className="chart-wrapper">
+        <div className="results-title-wrapper">
+          <span className="results-title">Social Tendencies</span>
+        </div>
+        <BarHorizontalChart
+            margins={{left: 120, right: 0, top: 0, bottom: 60}}
+            data={this.state.dataSets[2]}
+            width={330}
+            height={300}
+            chartSeries={this.chartSeries}
+            xTicks={[6]}
+            xDomain={[0, 1]}
+            y={this.y}
+            yScale = {'ordinal'}
+            showXGrid={false}
+            showYGrid={false}
+            showLegend={false}
+            svgClassName={"results-social"}
+          />
+      </div>
     </div>;
   }
 }
