eac9ecce17505506b9fb7e5b82800add5cb67dd6
Elan Weinstock <elanweinstock@Elans-MacBook-Pro.local>
2017-04-07 11:00:11 -0700

    more merge fixes

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,14 +3,12 @@ import testReducer from './test_reducer';
 import blurbsReducer from './blurbs_reducer';
 import personalityReducer from './personality_reducer';
 import sessionReducer from './session_reducer';
-import { reducer } from 'react-redux-oauth2';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   test: testReducer,
   blurbs: blurbsReducer,
-  personality: personalityReducer,
-  reducer
+  personality: personalityReducer
 });
 
 export default rootReducer;
