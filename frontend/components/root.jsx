6561011f475ff0cdbf78186558af9148fedfedc3
Kyumin Lee <kyumin133@gmail.com>
2017-04-07 22:39:02 -0700

    put reload before hashhistory push to reduce page blinking

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -16,8 +16,8 @@ class Root extends React.Component {
 
   redirectIfLoggedIn(nextState, replace) {
     if (window.currentUser) {
-      hashHistory.push("/home");
       location.reload();
+      hashHistory.push("/home");
     }
   }
 
