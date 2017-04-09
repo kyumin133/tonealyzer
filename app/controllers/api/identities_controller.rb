a99470f159b033ed834f045d1e53c8e6ecb7e4fc
aliciasavelly <alicia.savelly@gmail.com>
2017-04-09 15:17:23 -0700

    delete comments

--- app/controllers/api/identities_controller.rb
+++ app/controllers/api/identities_controller.rb
@@ -2,40 +2,18 @@ class Api::IdentitiesController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def new
-    # TODO is it username or email???
-
-    # unless params[:identity][:user]
-      # @identity = env['omniauth.identity']
-    # end
-
-    # if @identity.save
-    debugger
     @user = Identity.create!(email: params[:identity][:user][:username], name: params[:identity][:user][:username], password_digest: BCrypt::Password.create(params[:identity][:user][:password]))
     u = User.create!(provider: "identity", uid: @user.id, name: params[:identity][:user][:username])
 
     login(@user)
     render "api/users/show"
-    # else
-      # render json: @user.errors.full_messages, status: 422
-    # end
   end
 
   def create
-    # unless params[:identity][:email] # is it username or email???
-    #   @identity = env['omniauth.identity']
-    # end
-
-    # if @identity.save
-
-    debugger
     @user = Identity.create!(email: params[:identity][:user][:username], name: params[:identity][:user][:username], password_digest: BCrypt::Password.create(params[:identity][:user][:password]))
     u = User.create!(provider: "identity", uid: @user.id, name: params[:identity][:user][:username])
 
     login(@user)
-    debugger
     render "api/users/show"
-    # else
-      # render json: @user.errors.full_messages, status: 422
-    # end
   end
 end
