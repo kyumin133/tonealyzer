35650cc223902d4e0df2cee64ae345e84e3abdbc
aliciasavelly <alicia.savelly@gmail.com>
2017-04-09 21:38:01 -0700

    refactor and take notes on todos

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,4 +1,6 @@
 class Identity < OmniAuth::Identity::Models::ActiveRecord
+  # TODO add validations
+  
   def self.find_by_credentials(email, password)
     user = Identity.find_by_email(email)
     return user if user && user.is_password?(password)
