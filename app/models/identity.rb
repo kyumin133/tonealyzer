70b806ad7efb0654abeda5de737623573c196e00
aliciasavelly <alicia.savelly@gmail.com>
2017-04-12 13:26:21 -0700

    work on validations

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -2,6 +2,8 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   validates :email, :password_digest, presence: true
   validates :email, uniqueness: true
   # validates :password, length: {minimum: 6, allow_nil: true}
+  #
+  # attr_reader :password
 
   def self.find_by_credentials(email, password)
     user = Identity.find_by_email(email)
@@ -12,4 +14,8 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
+
+  # def password=(password)
+  #   @password = password
+  # end
 end
